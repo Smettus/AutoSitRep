ccbc32dfed56b7a4241e1ae716478f7da9060865
Smettus <68818239+Smettus@users.noreply.github.com>
2021-05-25 15:43:03 +0200

    Latest versions - 25/05

--- AutoSitRepv2.jl
+++ AutoSitRepv2.jl
@@ -1,6 +1,6 @@
 """
   Created by Tim De Smet
-  Last edit: 23/05/2021@1210
+  Last edit: 25/05/2021@1540
  
    1  7777777 555555
   111     777 55
@@ -15,45 +15,42 @@
 """
 # todo: dictionary, messenger bot
 
-global autosubmit = false # Automatically submit: 'false' -> 'true'
+autosubmit = false; # Automatically submit: 'false' -> 'true'
+baselink = "https://docs.google.com/forms/d/1wFMXmX4su9Q8GTagQk0j21K62AxgBtvGsQE6L7rEWRI"
 
 #	-------[ Change the next variables ]-------	 #
-lastName = "Your last name"
-firstName = "Your first name"
-promotion = ["175 POL", "160 SSMW", 1] # Select which one
-language = ["N", "F", 1]
-matricule = 696969 # Your matricule here
-address = "Your address"
-sports = "Your sports and duration"
+lastName = "Your last name";
+firstName = "Your first name";
+promotion = ["160 SSMW", "175 POL", 2]; # Select which one
+language = ["F", "N", 2];
+matricule = 696969; # Your matricule here
+address = "Your address here";
+sports = "Your Sports here";
 
 
 
 # -------[ Optional ]------- #
-whereAbouts = ["Home", "RMA", 1] # Select which one, if RMA is selected, address will change as well
-mailChecked = ["YES", "NO", 1]
-randsports = ["Run", "Bike", "Strength", "Walk", "Paaldansen", [45, 90], false] # For the liars, change 'false' in 'true' and it will generate a random sport of duration between 45/90min
+whereAbouts = ["RMA", "Other", 1]; # Select which one, if RMA is selected, address will change as well
+mailChecked = ["Yes", "No", 1];
+randsports = ["Run", "Bike", "Strength", "Walk", "Paaldansen", [45, 90], false]; # For the liars, change 'false' in 'true' and it will generate a random sport of duration between 45/90min
 medical = ["P (Completely Ops)", 
-		   "PS (I show symptoms of Corona)", 
-		   "PSE (People around me show symptoms of Corona)", 
-		   "Q (Quarantaine) when you are positive tested", 
-		   "I (Isolation) waiting for the result", 
+		   "PS (I show symptoms of Corona and confirmed)", 
+		   "PSE (My entourage shows symptoms of Corona and confirmed)", 
+		   "Q (Quarantaine)", 
 		   "Positive test", 
-		   "Negative test", 1]
-questions = "None."
-remarques = ""
+		   "Negative test", 
+		   "AGR/AMS (no Corona)", 1];
+questions = "None.";
+remarques = "";
 
 #	!-------[ CHANGE WITH CAUTION ]-------!	 #
 using Dates
-global entry = [1981765836, 261023013, 767032974, 1322242599, 239541400, 202815844, 2072704141, 1841305855, 2124830854, 690739259, 1110877319, 1403071410, 1380738579, 1304546116]
-global baselink = "https://docs.google.com/forms/d/1wFMXmX4su9Q8GTagQk0j21K62AxgBtvGsQE6L7rEWRI"
-global answer = [-1, -1, lastName, firstName, promotion, language, 
+entry = [2098902328, 735224320, 110406996, 970617568, 1416870757, 533365005, 791227354, 1716620882, 529903153, 946171575, 712245977, 404233749, 1380738579, 544884552]
+baselink = "https://docs.google.com/forms/d/1wFMXmX4su9Q8GTagQk0j21K62AxgBtvGsQE6L7rEWRI"
+answer = [-1, -1, lastName, firstName, promotion, 
 				matricule, whereAbouts, address, mailChecked, 
-				sports, medical, questions, remarques]
+				sports, medical, questions, remarques, language]
 function createLink()
-	global entry
-	global baselink
-	global answer
-
 	if autosubmit
 		link = baselink*"/formResponse?"
 	else
@@ -70,6 +67,9 @@ function createLink()
 		answer[2]*=":"*"0"*string(Dates.minute(now()))
 	else
 		answer[2]*=":"*string(Dates.minute(now()))
+	end
+	if whereAbouts[whereAbouts[end]] == "RMA"
+		answer[8] = "RMA"
 	end
 	if randsports[end]
 		answer[11] = "$(randsports[rand(1:(length(randsports)-2))])"*"-"*"$(rand(randsports[end-1][1]:randsports[end-1][2]))"*"min"
--- AutoSitRepv2.js
+++ AutoSitRepv2.js
@@ -1,6 +1,6 @@
 /*
   Created by Tim De Smet
-  Last edit: 24/05/2021@2100
+  Last edit: 25/05/2021@1540
  
    1  7777777 555555
   111     777 55
@@ -19,33 +19,34 @@ var baselink = "https://docs.google.com/forms/d/1wFMXmX4su9Q8GTagQk0j21K62AxgBtv
 //	-------[ Change the next variables ]-------	 //
 var lastName = "Your last name";
 var firstName = "Your first name";
-var promotion = ["175 POL", "160 SSMW", 1]; // Select which one
-var language = ["N", "F", 1];
+var promotion = ["160 SSMW", "175 POL", 2]; // Select which one
+var language = ["F", "N", 2];
 var matricule = 696969; // Your matricule here
-var address = "Your address";
-var sports = "Your sports and duration";
+var address = "Your address here";
+var sports = "Your Sports here";
 
 
 
 // -------[ Optional ]------- //
-var whereAbouts = ["Home", "RMA", 1]; // Select which one, if RMA is selected, address will change as well
-var mailChecked = ["YES", "NO", 1];
+var whereAbouts = ["RMA", "Other", 1]; // Select which one, if RMA is selected, address will change as well
+var mailChecked = ["Yes", "No", 1];
 var randsports = ["Run", "Bike", "Strength", "Walk", "Paaldansen", [45, 90], false]; // For the liars, change 'false' in 'true' and it will generate a random sport of duration between 45/90min
 var medical = ["P (Completely Ops)", 
-		   "PS (I show symptoms of Corona)", 
-		   "PSE (People around me show symptoms of Corona)", 
-		   "Q (Quarantaine) when you are positive tested", 
-		   "I (Isolation) waiting for the result", 
+		   "PS (I show symptoms of Corona and confirmed)", 
+		   "PSE (My entourage shows symptoms of Corona and confirmed)", 
+		   "Q (Quarantaine)", 
 		   "Positive test", 
-		   "Negative test", 1];
+		   "Negative test", 
+		   "AGR/AMS (no Corona)", 1];
 var questions = "None.";
 var remarques = "";
 
 //	!-------[ CHANGE WITH CAUTION ]-------!	 //
-var entry = [1981765836, 261023013, 767032974, 1322242599, 239541400, 202815844, 2072704141, 1841305855, 2124830854, 690739259, 1110877319, 1403071410, 1380738579, 1304546116];
-var answer = [-1, -1, lastName, firstName, promotion, language, 
+//[1981765836, 261023013, 767032974, 1322242599, 239541400, 202815844, 2072704141, 1841305855, 2124830854, 690739259, 1110877319, 1403071410, 1380738579, 1304546116] previously
+var entry = [2098902328, 735224320, 110406996, 970617568, 1416870757, 533365005, 791227354, 1716620882, 529903153, 946171575, 712245977, 404233749, 1380738579, 544884552];
+var answer = [-1, -1, lastName, firstName, promotion,
 				matricule, whereAbouts, address, mailChecked, 
-				sports, medical, questions, remarques];
+				sports, medical, questions, remarques, language];
 
 function createLink() {
 	if (autosubmit) {
@@ -71,9 +72,12 @@ function createLink() {
 	else {
 		answer[1]+=":"+d.getMinutes().toString();
 	}
+	if (whereAbouts[whereAbouts[whereAbouts.length-1]-1] == "RMA") {
+		answer[7] = "RMA"
+	}
 	if (randsports[randsports.length-1]) {
 		//Math.floor(Math.random() * array.length)
-		//todo
+		//todo, doesnt work yet
 		answer[10] = "${randsports[rand(1:(randsports.length-2))]}"+"-"+"${rand(randsports[randsports.length-2-1][1]:randsports[randsports.length-2-1][2])}"+"min";
 	}
 
@@ -97,19 +101,4 @@ function createLink() {
 	}
 
  	document.getElementById("link").innerHTML = link;
-}
-
-function test() {
-
-	var yeet = "yeet";
-	var derp = "derp";
-	var mkay = derp+yeet;
-
-	console.log(mkay);
-
-	var d = new Date();
-
-	a = d.getHours();
-	console.log(a.toString());
-	console.log(a.toString().length);
 }
